181b071eb4960d39d744143d9a57b59fe049d919
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-06 03:17:22 +0000

    .

---
+++ main.cs
@@ -0,0 +1,15 @@
+// Created by: Ekaterina
+// Created on: Oct 2022
+//
+// This program displays, "Hello, World!"
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nDone.");
+    }
+}
